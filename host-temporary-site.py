7f7d49a5af2018165d98ec42a9d6034baee166c6
Richard Challis <richard.challis@sanger.ac.uk>
2024-06-11 15:27:40 +0100

    catch 404s

--- host-temporary-site.py
+++ host-temporary-site.py
@@ -2,7 +2,9 @@
 
 import functools
 import http.server
+import os
 import socketserver
+import subprocess
 
 # Set the directory containing the files you want to serve
 directory = "./placeholder"
@@ -10,8 +12,36 @@ directory = "./placeholder"
 # Set the port number for the web server
 port = 8000
 
+
 # Create a handler to serve the files
-handler = functools.partial(http.server.SimpleHTTPRequestHandler, directory=directory)
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # If the requested file doesn't exist, serve index.html instead
+        if not os.path.exists(self.translate_path(self.path)):
+            self.path = "/index.html"
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+
+handler = functools.partial(MyHandler, directory=directory)
+
+# Close any existing process using the specified port
+
+
+def close_process_using_port(port):
+    try:
+        subprocess.run(
+            ["lsof", "-ti", f":{port}", "-sTCP:LISTEN", "-n"],
+            check=True,
+            capture_output=True,
+        )
+    except subprocess.CalledProcessError as e:
+        pid = e.stdout.decode().strip()
+        subprocess.run(["kill", pid])
+
+
+# Close any existing process using the specified port
+close_process_using_port(port)
+
 
 # Start the web server
 with socketserver.TCPServer(("", port), handler) as httpd:
